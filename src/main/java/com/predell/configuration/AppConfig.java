0a2d98b081c00b0c4ee433bb0e7f1b8a171d2e46
karl <krenou@predell.fr>
2024-11-26 14:49:59 +0100

    feat: update version and add Bcc header to email

---
+++ src/main/java/com/predell/configuration/AppConfig.java
@@ -0,0 +1,59 @@
+package com.predell.configuration;
+
+import org.apache.commons.configuration2.Configuration;
+import org.apache.commons.configuration2.XMLConfiguration;
+import org.apache.commons.io.FilenameUtils;
+
+import java.util.Properties;
+
+public class AppConfig {
+
+	private volatile static AppConfig objectInstance = null;
+
+	private Configuration appConfig;
+	private Properties charSetMap = null;
+
+	private AppConfig() {
+        this.appConfig = new XMLConfiguration();
+    }
+
+	public static AppConfig getInstance() {
+		if (objectInstance == null) {
+			synchronized (AppConfig.class) {
+				if (objectInstance == null) {
+					objectInstance = new AppConfig();
+				}
+			}
+		}
+		return objectInstance;
+	}
+
+	public Properties getCharSetMap() {
+		if (charSetMap == null) {
+			synchronized (AppConfig.class) {
+				if (charSetMap == null) {
+					charSetMap = new Properties();
+					String[] arrStr = appConfig.getStringArray("appSettings.charSetFallback");
+					for (int i = 0; i < arrStr.length; i++) {
+						String temp = arrStr[i];
+						String parentCharSet = temp.substring(temp.indexOf(':') + 1, temp.length());
+						temp = temp.substring(0, temp.indexOf(':'));
+						String[] arrChild = temp.split(",");
+						for (int j = 0; j < arrChild.length; j++) {							
+							charSetMap.setProperty(arrChild[j].toLowerCase(), parentCharSet);
+						}
+					}
+				}
+			}
+		}
+		return charSetMap;
+	}
+
+	public boolean isImageFormat(String fileName) {
+		if (fileName != null && !fileName.isEmpty()) {
+			String[] arrImageFormat = appConfig.getStringArray("appSettings.imageFileFormats");
+			return FilenameUtils.isExtension(fileName.toLowerCase(), arrImageFormat);
+		}
+		return false;
+	}	
+}
